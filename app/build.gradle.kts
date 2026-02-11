c23c9749180a1a82259ef3a5ea132e1402d65749
Ashutosh Shinde <ashutoshinde23@gmail.com>
2026-02-11 16:12:01 +0530

    v0.6 release-readiness

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.projectlyra.app"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.projectlyra.app"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "0.1.0"
 
